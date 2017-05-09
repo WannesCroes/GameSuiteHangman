b03c16399502e63617f00f62fc61f2b300e85f69
LAPTOP-H4FA1527\Thijs Vercammen <thijs.vercammen@.student.ucll.be>
2017-05-09 10:59:45 +0200

    Afwerking Story 02. Doener Lennert, Denker Thijs

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -11,11 +11,14 @@ public class Launcher {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
 		Speler speler = new Speler(naam);
 
+		
+		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		
 		int x = Integer.parseInt(JOptionPane.showInputDialog("x coordinaat van het punt:"));
 		int y = Integer.parseInt(JOptionPane.showInputDialog("y coordinaat van het punt:"));
 		Punt punt = new Punt(x,y);
 		
-		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt.toString(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 	}
 
 }
