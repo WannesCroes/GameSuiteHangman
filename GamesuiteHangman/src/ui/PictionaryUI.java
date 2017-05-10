78a569490422077442695e1b89b629f9f491ef9d
r0579283 <wannes.croes@student.ucll.be>
2017-05-10 10:29:12 +0200

    PictionaryUi voor story 11  binnenhalen

--- GamesuiteHangman/src/ui/PictionaryUI.java
+++ GamesuiteHangman/src/ui/PictionaryUI.java
@@ -1,9 +1,11 @@
 package ui;
 
 import domain.Speler;
+import domain.Tekening;
 
 public class PictionaryUI {
 	Speler speler;
+	Tekening tekening;
 	
 	public PictionaryUI(Speler speler){
 		this.speler = speler;
