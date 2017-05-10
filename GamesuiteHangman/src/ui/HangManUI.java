6c911e0156bafe6388d1d1549c4d33d1dc60f89c
LAPTOP-H4FA1527\Thijs Vercammen <thijs.vercammen@.student.ucll.be>
2017-05-10 11:04:35 +0200

    Story 13 - denker: Mathias, Doener: thijs

---
+++ GamesuiteHangman/src/ui/HangManUI.java
@@ -0,0 +1,17 @@
+package ui;
+
+import domain.Speler;
+import domain.WoordenLijst;
+
+public class HangManUI {
+	
+	Speler speler;
+	WoordenLijst woordenlijst;
+	
+	public HangManUI(Speler speler, WoordenLijst woordenlijst){
+		
+	}
+	public void play(){
+		
+	}
+}
