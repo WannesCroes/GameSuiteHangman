ebf4e323ec997590c8b89ae99283a7ccb72fafc4
Thijs Vercammen <thijs.vercammen@.student.ucll.be>
2017-05-11 15:46:38 +0200

    Story 15 - Test

--- GamesuiteHangman/test/domain/AllTests.java
+++ GamesuiteHangman/test/domain/AllTests.java
@@ -5,7 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PuntTest.class, SpelerTest.class, CirkelTest.class, RechthoekTest.class, LijnStukTest.class, WoordenLijstTest.class})
+@SuiteClasses({ PuntTest.class, SpelerTest.class, CirkelTest.class, RechthoekTest.class, LijnStukTest.class,
+	WoordenLijstTest.class, HangManTest.class, DriehoekTest.class, HintLetterTest.class, HintWoordTest.class, OmhullendeTest.class,
+	TekeningHangManTest.class, TekeningTest.class})
 public class AllTests {
 
 }
